ddb1b70c663d7ab92ea2b9a5c9b3e96b6d2d9140
Erde321 <165824066+Erde321@users.noreply.github.com>
2024-04-02 20:22:21 +0200

    Update sendtext.py

--- sendtext.py
+++ sendtext.py
@@ -8,11 +8,11 @@ def send_whatsapp_message(message):
     whatsapp.send_message(message)
 
 if __name__ == "__main__":
-    # Überprüfe, ob ein Übergabeparameter vorhanden ist
+    # Check if there is a command-line argument
     if len(sys.argv) != 2:
         print("Usage: python your_script.py 'message'")
         sys.exit(1)
     
-    # Rufe die Funktion auf, um die WhatsApp-Nachricht zu senden
+    # Call the function to send the WhatsApp message
     message = sys.argv[1]
     send_whatsapp_message(message)
